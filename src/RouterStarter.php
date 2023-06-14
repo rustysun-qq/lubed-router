8eaf16ca9ee3af04b8f614ccb3ecffe17170e478
cjj <cjj8110_202302@126.com>
2023-06-14 12:27:52 +0800

    init for 0.2.0

--- src/RouterStarter.php
+++ src/RouterStarter.php
@@ -2,21 +2,18 @@
 namespace Lubed\Router;
 
 use Lubed\Supports\Starter;
-use Lubed\Container\Container;
+use Lubed\Utils\Registry;
 
-final class RouterStarter implements Starter
-{
-	private $loader;
-	private $app;
+final class RouterStarter implements Starter {
+    private $loader;
+    private $app;
 
-	public function __construct(string $config_file, Container &$app)
-	{
-		$this->loader = new DefaultRouterLoader($config_file);
-		$this->app=$app;
-	}
+    public function __construct(string $config_file) {
+        $this->loader=new DefaultRouterLoader($config_file);
+    }
 
-	public function start()
-	{
-		$this->app->instance('lubed_router_router', $this->loader->load());
-	}
+    public function start() {
+        $registry=Registry::getInstance();
+        $registry->set('lubed_router_router', $this->loader->load());
+    }
 }
